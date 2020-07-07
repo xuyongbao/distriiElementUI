bbb535a47bcdc9d58cf573809e7f1dc21719b355
xuyongbao <13916688245@163.com>
2020-07-07 14:37:06 +0800

    add

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+const About = Vue.extend({
+  // 类型推断已启用
+  name: 'About',
+})
+
+export default About;
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Vue from 'vue'
+
+const Home = Vue.extend({
+  // 类型推断已启用
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+})
+
+export default Home;
+</script>
